a132471a3eda3453c3379b82339ef26098b0fc76
sunqilong <1441101265@qq.com>
2020-10-30 15:11:22 +0800

    修改junit引用包，修改查询调用方法

--- shop-product/src/test/java/com/kiss/shop/product/service/BrandServiceTest.java
+++ shop-product/src/test/java/com/kiss/shop/product/service/BrandServiceTest.java
@@ -1,25 +1,26 @@
 package com.kiss.shop.product.service;
 
-import org.junit.jupiter.api.Test;
+import com.kiss.common.utils.PageUtils;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
-class BrandServiceTest
-{
+public class BrandServiceTest {
 
     @Autowired
     BrandService brandService;
 
-
-
     @Test
-    void queryPage()
-    {
-        System.out.println(brandService.list());
-
+    public void queryPage() {
+        Map<String, Object> map = new HashMap<>();
+        PageUtils pageUtils = brandService.queryPage(map);
+        System.out.println(pageUtils);
     }
 }
